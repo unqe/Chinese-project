c153e3a416bd74be20c86cc842b7745b4fb7dff1
unqe <orabo2acc@gmail.com>
2026-02-24 11:52:42 +0000

    Fix collectstatic: use CompressedStaticFilesStorage, add .python-version

--- despair/settings/base.py
+++ despair/settings/base.py
@@ -148,7 +148,7 @@ MODELTRANSLATION_LANGUAGES = ("en", "zh-hans")
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
